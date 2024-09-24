d0ca7ca0e7223e322e4ccc93cca24d9a35102d41
brunascauri <brunascauri@hotmail.com>
2024-09-24 16:48:12 -0300

    WIP: authentication method

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,42 @@
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> signUpUser({
+    //future because it will be asynchronous
+    required String username,
+    required String email,
+    required String password,
+    required String bio,
+    // required Uint8List file,
+  }) async {
+    String res = 'Some error ocurred';
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
+        UserCredential cred = _auth.createUserWithEmailAndPassword(
+            email: email, password: password) as UserCredential; //1:13:20
+        // print(cred.user.displayName);
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          // 'password': password,
+          'bio': bio,
+          'followers': [],
+          'following': []
+        });
+        res = 'success';
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
