249c250ad26584ea7bb6a3cfafea4dbfa2feb5c7
brunascauri <brunascauri@hotmail.com>
2026-01-08 13:58:55 -0300

    chore: update comment

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -31,7 +31,7 @@ class AuthMethods {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
           email: email, password: password
         );
-        print(cred.user!.uid);
+        //currently not uploading to firestore as that requires a paid account :D
         // String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
 
         await _firestore.collection('users').doc(cred.user!.uid).set({
